40cca193f03d54b3a09d1174b451ab37ecc04087
Jakub Szczepaniak <jakub.szczepaniak@slideworx.com>
2017-07-14 12:06:36 +0200

    Transfer class refactor.

--- src/TransferObj.js
+++
@@ -1,7 +0,0 @@
-const fieldSplitters = require('./fieldSplitters');
-
-module.exports = function TransferObj(transferData) {
-  for (const key in transferData) {
-    this[key] = fieldSplitters[key] ? fieldSplitters[key](transferData[key]) : transferData[key];
-  }
-};
